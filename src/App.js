39076e22aa372adde73f167f7c8c33da8d75c4b4
MatasKa <matas.k2002@gmail.com>
2023-05-14 15:42:03 +0200

    Add styling to Login view

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TemperatureComponent from "./TemperatureComponent";
 import Login from "./Login";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
+    <div className="h-full bg-white">
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-
 module.exports = {
-  purge: [],
-  darkMode: false,
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: "#FF0000",
-        secondary: "#00FF00",
-      },
-      fontFamily: {
-        sans: ["Arial", "Helvetica", "sans-serif"],
-      },
-    },
+    plugins: [require("@tailwindcss/forms")],
   },
-  variants: {},
-  plugins: [],
 };
