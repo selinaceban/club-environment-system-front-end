1488362063eb4fc3ca122041c848821f2f3ab48b
MatasKa <matas.k2002@gmail.com>
2023-05-14 15:42:03 +0200

    Add styling to Login view

--- Website/src/Login.js
+++ Website/src/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import "./Login.css";
 import { useNavigate } from "react-router-dom";
+import "./App.css";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -29,24 +29,50 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="bg-blue-500 text-white p-4">
-      <h1>Login</h1>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={handleUsernameChange} />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-      </label>
-      <br />
-      <button type="submit">Log In</button>
-    </form>
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h1 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Login
+        </h1>
+      </div>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={handleSubmit}>
+          <label
+            for="username"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Username:
+          </label>
+          <div className="mt-2">
+            <input
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              type="text"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+          </div>
+          <br />
+          <label className="block text-sm font-medium leading-6 text-gray-900">
+            Password:
+          </label>
+          <div className="mt-2">
+            <input
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <br />
+          <button
+            className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            type="submit"
+          >
+            Log In
+          </button>
+        </form>
+      </div>
+    </div>
   );
 };
 
