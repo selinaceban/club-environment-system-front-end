926c67077c557cccaf3acd4561048fa06ec3c02b
MatasKa <matas.k2002@gmail.com>
2023-05-14 15:58:03 +0200

    Add login Styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 
 const App = () => {
   return (
-    <div className="h-full bg-white">
+    <div>
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
