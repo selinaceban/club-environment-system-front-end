239863b308f68339321c49960a61c3fb8b985e5c
MatasKa <matas.k2002@gmail.com>
2023-05-15 15:18:35 +0200

    Add Limits and styling to Humidity and CO2

--- Website/src/Limits.js
+++ Website/src/Limits.js
@@ -70,28 +70,86 @@ const Limits = () => {
             </div>
 
             <p class="mt-6 text-lg leading-8 text-gray-400 text-center">
-              Recommended Limit: 20 °C
+              Recommended Limit: 18 °C
             </p>
           </div>
         </div>
-        <div class="max-w-2xl">
-          <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center">
-            Humidity
-          </h2>
-          <p class="mt-6 text-lg leading-8 text-gray-600 text-center">
-            Libero fames augue nisl porttitor nisi, quis. Id ac elit odio vitae
-            elementum enim vitae ullamcorper suspendisse.
-          </p>
+        <div class=" border max-w-2xl space-y-32 rounded-md px-3.5 py-2.5 text-sm ">
+          <div>
+            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center">
+              Humidity
+            </h2>
+          </div>
+          <div>
+            <p class="mt-6 text-lg leading-8 text-black-600 text-center">
+              Upper Limit
+            </p>
+            <div class="relative">
+              <span class="absolute inset-y-2 right-2 flex items-center pr-1 text-gray-500">
+                %
+              </span>
+              <input
+                type="text"
+                class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              ></input>
+            </div>
+
+            <p class="mt-6 text-lg leading-8 text-gray-400 text-center">
+              Recommended Limit: 60 %
+            </p>
+          </div>
+
+          <div>
+            <p class="mt-6 text-lg leading-8 text-black-600 text-center">
+              Lower Limit
+            </p>
+            <div class="relative">
+              <span class="absolute inset-y-2 right-2 flex items-center pr-1 text-gray-500">
+                %
+              </span>
+              <input
+                type="text"
+                class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              ></input>
+            </div>
+
+            <p class="mt-6 text-lg leading-8 text-gray-400 text-center">
+              Recommended Limit: 30 %
+            </p>
+          </div>
         </div>
-        <div class="max-w-2xl">
-          <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center">
-            CO2
-          </h2>
-          <p class="mt-6 text-lg leading-8 text-gray-600">
-            Libero fames augue nisl porttitor nisi, quis. Id ac elit odio vitae
-            elementum enim vitae ullamcorper suspendisse.
-          </p>
+        <div class=" border max-w-2xl space-y-32 rounded-md px-3.5 py-2.5 text-sm ">
+          <div>
+            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center">
+              CO2
+            </h2>
+          </div>
+          <div>
+            <p class="mt-6 text-lg leading-8 text-black-600 text-center">
+              Upper Limit
+            </p>
+            <div class="relative">
+              <span class="absolute inset-y-2 right-2 flex items-center pr-1 text-gray-500">
+                ppm
+              </span>
+              <input
+                type="text"
+                class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              ></input>
+            </div>
+            <p class="mt-6 text-lg leading-8 text-gray-400 text-center">
+              Recommended Limit: 800ppm
+            </p>
+          </div>
         </div>
+      </div>
+      <div class="flex justify-center ">
+        <button
+          type="submit"
+          class="mt-7 w-96 h-14 items-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Save Limits
+        </button>
       </div>
     </div>
   );
