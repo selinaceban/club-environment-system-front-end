be7d4f4afacfcd1bb28de6dcddfbc32a09a6d8f2
MatasKa <matas.k2002@gmail.com>
2023-05-19 17:19:01 +0200

    Add info to front page

--- Website/src/FrontPageComponent.js
+++ Website/src/FrontPageComponent.js
@@ -17,13 +17,11 @@ const FrontPageComponent = () => {
         </div>
         <div class="mx-auto max-w-2xl sm:py-48 lg:py-56">
           <div class="text-center">
-            <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            <h1 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">
               Valhalla
             </h1>
-            <p class="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
+            <p class="mt-6 text-lg leading-8 text-gray-400">
+              Address: Kamtjatka 11, 8700 Horsens, Denmark
             </p>
           </div>
         </div>
