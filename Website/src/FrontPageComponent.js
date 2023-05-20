5227cf98b30013b810e2e536fab960d0329fa52d
MatasKa <matas.k2002@gmail.com>
2023-05-20 12:23:56 +0200

    Edit styling

--- Website/src/FrontPageComponent.js
+++ Website/src/FrontPageComponent.js
@@ -6,11 +6,11 @@ const FrontPageComponent = () => {
       <img
         src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
         alt=""
-        class="absolute h-screen inset-0 -z-10 w-full object-cover object-right md:object-center"
+        class="absolute h-screen inset-0 z-[-10] w-full object-cover object-right md:object-center"
       ></img>
-      <div class="relative isolate px-6 lg:px-8">
+      <div class="relative z-[-10] isolate px-6 lg:px-8">
         <div
-          class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          class="absolute z-[-10] inset-x-0 -top-40 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
         >
           <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"></div>
@@ -24,12 +24,12 @@ const FrontPageComponent = () => {
               Address: Kamtjatka 11, 8700 Horsens, Denmark
             </p>
           </div>
-        </div>
-        <div
-          class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-          aria-hidden="true"
-        >
-          <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"></div>
+          <div
+            class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+            aria-hidden="true"
+          >
+            <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"></div>
+          </div>
         </div>
       </div>
     </div>
