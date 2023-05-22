5f3e064032ca45586d10d7b0687ee4d17649c7d3
MatasKa <matas.k2002@gmail.com>
2023-05-22 16:48:14 +0200

    Edit Margins

--- Website/src/FrontPageComponent.js
+++ Website/src/FrontPageComponent.js
@@ -13,15 +13,13 @@ const FrontPageComponent = () => {
         <div
           class="absolute z-[-10] inset-x-0 -top-40 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
-        >
-          <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"></div>
-        </div>
-        <div class="mx-auto max-w-2xl sm:py-48 lg:py-56">
+        ></div>
+        <div class="mx-auto max-w-2xl sm:py-48 lg:py-96">
           <div class="text-center">
             <h1 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">
               Valhalla
             </h1>
-            <p class="mt-6 text-lg leading-8 text-gray-400">
+            <p class="mt-12 text-lg leading-8 text-gray-400">
               Address: Kamtjatka 11, 8700 Horsens, Denmark
             </p>
           </div>
@@ -29,7 +27,7 @@ const FrontPageComponent = () => {
             class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
             aria-hidden="true"
           >
-            <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"></div>
+            {" "}
           </div>
         </div>
       </div>
