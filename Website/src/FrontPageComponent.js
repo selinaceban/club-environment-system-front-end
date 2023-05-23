d12a56c762dc77bd4f8f678e9cebef5e9aa7bd64
MatasKa <matas.k2002@gmail.com>
2023-05-23 22:06:46 +0200

    Edit the layout

--- Website/src/FrontPageComponent.js
+++ Website/src/FrontPageComponent.js
@@ -40,17 +40,57 @@ const FrontPageComponent = () => {
     <div>
       <div>
         <div class="absolute flex h-2/5 items-end">
-          <h1 class="absolute flex justify-center w-screen font-bold tracking-tight text-gray-900 text-6xl text-center">
+          <h1 class="ml-96 font-bold tracking-tight text-gray-900 text-7xl text-center">
             Valhalla
           </h1>
         </div>
         <div class="absolute flex h-1/2 items-end">
-          <p class="flex justify-center w-screen text-xl text-gray-500 ">
+          <p class="ml-52 text-2xl text-gray-700 ">
             Horsens Student Bar, the place to be on the Friday nights!
           </p>
         </div>
+        <div>
+          <a
+            href="#"
+            class="absolute top-[70%] left-[23%] z-10 rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700"
+          >
+            To Readings
+          </a>
+        </div>
       </div>
       <div>
+        <img
+          src={image1}
+          class="absolute top-[12%] left-[55%] w-[350px] h-[250px] rounded-lg "
+          alt="Image 1"
+        ></img>
+        <img
+          src={image2}
+          class="absolute top-[40%] left-[55%] w-[350px] h-[250px] rounded-lg "
+          alt="Image 2"
+        ></img>
+        <img
+          src={image3}
+          class="absolute top-[68%] left-[55%] w-[350px] h-[250px] rounded-lg "
+          alt="Image 3"
+        ></img>
+        <img
+          src={image4}
+          class="absolute top-[8%] left-[75%] w-[350px] h-[250px] rounded-lg "
+          alt="Image 4"
+        ></img>
+        <img
+          src={image5}
+          class="absolute top-[36%] left-[75%] w-[350px] h-[250px] rounded-lg "
+          alt="Image 5"
+        ></img>
+        <img
+          src={image6}
+          class="absolute top-[64%] left-[75%] w-[350px] h-[250px] rounded-lg "
+          alt="Image 6"
+        ></img>
+      </div>
+      {/* <div>
         <div class="flex items-center justify-center w-screen pt-4">
           <div aria-hidden="true" class="pointer-events-none">
             <div class="flex items-center space-x-6 lg:space-x-32">
@@ -106,7 +146,7 @@ const FrontPageComponent = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
