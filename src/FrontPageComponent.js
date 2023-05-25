c6ece75aa170c3ad36084cba844d736422eab8f0
MatasKa <matas.k2002@gmail.com>
2023-05-25 16:00:14 +0200

    Restructured the styling

--- src/FrontPageComponent.js
+++ src/FrontPageComponent.js
@@ -8,58 +8,118 @@ import image6 from "./images/Valhalla6.jpg";
 
 const FrontPageComponent = () => {
   return (
-    <div>
-      <div>
-        <div class="absolute flex h-2/5 items-end">
-          <h1 class="ml-96 font-bold tracking-tight text-gray-900 text-7xl text-center">
-            Valhalla
-          </h1>
-        </div>
-        <div class="absolute flex h-1/2 items-end">
-          <p class="ml-52 text-2xl text-gray-700 ">
-            Horsens Student Bar, the place to be on the Friday nights!
-          </p>
-        </div>
-        <div>
-          <a
-            href="ReadingsComponent"
-            class="absolute top-[70%] left-[23%] z-10 rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700"
+    <div class="relative overflow-hidden bg-white">
+      <div class="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pt-40">
+        <div class="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-0">
+          <div class="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pt-40">
+            <div class="sm:max-w-lg">
+              <h1 class="ml-36 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                Valhalla
+              </h1>
+              <p class="mt-4 text-xl text-gray-500">
+                Horsens Student Bar, the place to be on the Friday nights!
+              </p>
+            </div>
+            <div class="mt-20">
+              <a
+                href="ReadingsComponent"
+                class="ml-9  z-10 rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-center font-medium text-white hover:bg-indigo-700"
+              >
+                To Readings
+              </a>
+              <a
+                href="LimitsComponent"
+                class="ml-4 z-10 rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700"
+              >
+                To Limits
+              </a>
+              <a
+                href="LogTableComponent"
+                class="ml-4 z-10 rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700"
+              >
+                To Logs
+              </a>
+            </div>
+          </div>
+          <div
+            aria-hidden="true"
+            class="pointer-events-none lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl"
           >
-            To Readings
-          </a>
+            <div class="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
+              <div class="flex items-center space-x-6 lg:space-x-8"></div>
+              <div class="grid grid-cols-2 gap-4 ">
+                <div class="col-span-1">
+                  <img
+                    src={image1}
+                    alt="Image 1"
+                    class="rounded-lg ml-28 mt-10"
+                  ></img>
+                  <img
+                    src={image2}
+                    alt="Image 2"
+                    class="rounded-lg ml-28 my-4"
+                  ></img>
+                  <img
+                    src={image3}
+                    alt="Image 3"
+                    class="rounded-lg ml-28 my-4"
+                  ></img>
+                </div>
+                <div class="col-span-1">
+                  <img
+                    src={image4}
+                    alt="Image 4"
+                    class="rounded-lg ml-28 "
+                  ></img>
+                  <img
+                    src={image5}
+                    alt="Image 5"
+                    class="rounded-lg ml-28 my-4"
+                  ></img>
+                  <img
+                    src={image6}
+                    alt="Image 6"
+                    class="rounded-lg ml-28 my-4"
+                  ></img>
+                </div>
+
+                {/* <div>
+                <img
+                  src={image1}
+                  class="rounded-lg"
+                  alt="./images/Valhalla.jpg"
+                ></img>
+                <img
+                  src={image2}
+                  class="absolute top-[40%] left-[55%] w-[350px] h-[250px] rounded-lg "
+                  alt="./images/Valhalla2.jpg"
+                ></img>
+                <img
+                  src={image3}
+                  class="absolute top-[68%] left-[55%] w-[350px] h-[250px] rounded-lg "
+                  alt="./images/Valhalla3.jpg"
+                ></img>
+                <img
+                  src={image4}
+                  class="absolute top-[8%] left-[75%] w-[350px] h-[250px] rounded-lg "
+                  alt="./images/Valhalla4.jpg"
+                ></img>
+                <img
+                  src={image5}
+                  class="absolute top-[36%] left-[75%] w-[350px] h-[250px] rounded-lg "
+                  alt="./images/Valhalla5.jpg"
+                ></img>
+                <img
+                  src={image6}
+                  class="absolute top-[64%] left-[75%] w-[350px] h-[250px] rounded-lg "
+                  alt="./images/Valhalla6.jpg"
+                ></img>
+              </div>
+            </div> */}
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div>
-        <img
-          src={image1}
-          class="absolute top-[12%] left-[55%] w-[350px] h-[250px] rounded-lg "
-          alt="./images/Valhalla.jpg"
-        ></img>
-        <img
-          src={image2}
-          class="absolute top-[40%] left-[55%] w-[350px] h-[250px] rounded-lg "
-          alt="./images/Valhalla2.jpg"
-        ></img>
-        <img
-          src={image3}
-          class="absolute top-[68%] left-[55%] w-[350px] h-[250px] rounded-lg "
-          alt="./images/Valhalla3.jpg"
-        ></img>
-        <img
-          src={image4}
-          class="absolute top-[8%] left-[75%] w-[350px] h-[250px] rounded-lg "
-          alt="./images/Valhalla4.jpg"
-        ></img>
-        <img
-          src={image5}
-          class="absolute top-[36%] left-[75%] w-[350px] h-[250px] rounded-lg "
-          alt="./images/Valhalla5.jpg"
-        ></img>
-        <img
-          src={image6}
-          class="absolute top-[64%] left-[75%] w-[350px] h-[250px] rounded-lg "
-          alt="./images/Valhalla6.jpg"
-        ></img>
       </div>
     </div>
   );
