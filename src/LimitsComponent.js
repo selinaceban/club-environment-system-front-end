d38cf279a2d1ef4247e0a2bc44815eda345a71b2
Daniel <39463084+dosermind@users.noreply.github.com>
2023-05-28 12:55:02 +0200

    +

--- src/LimitsComponent.js
+++ src/LimitsComponent.js
@@ -98,14 +98,15 @@ const Limits = () => {
   useEffect(() => {
     const fetchLimitsData = async () => {
       try {
-        const response = await fetch("limits.json");
+        const response = await fetch('https://web-api-j4b5eryumq-ez.a.run.app/limits');
         if (response.ok) {
+          console.log("test");
           const data = await response.json();
-          setLTemp(data.LTemp);
-          setUTemp(data.UTemp);
-          setLHum(data.LHum);
-          setUHum(data.UHum);
-          setCO2(data.CO2);
+          setLTemp(data.minTemperature);
+          setUTemp(data.maxTemperature);
+          setLHum(data.minHumidity);
+          setUHum(data.maxHumidity);
+          setCO2(data.maxCo2);
           console.log(data);
         } else {
           throw new Error("Failed to fetch limits data");
@@ -121,23 +122,23 @@ const Limits = () => {
   return (
     <div>
    
-      <div class="bg-white py-24 sm:py-16">
-        <h1 class="mb-16 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center">
+      <div className="bg-white py-24 sm:py-16">
+        <h1 className="mb-16 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center">
           Limits
         </h1>
-        <div class="mx-auto grid max-w-4xl gap-x-6 gap-y-20 px-6 lg:px-1 xl:grid-cols-3">
-          <div class=" border max-w-2xl space-y-16 rounded-md px-3.5 py-2.5 text-sm ">
+        <div className="mx-auto grid max-w-4xl gap-x-6 gap-y-20 px-6 lg:px-1 xl:grid-cols-3">
+          <div className=" border max-w-2xl space-y-16 rounded-md px-3.5 py-2.5 text-sm ">
             <div>
-              <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center">
+              <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center">
                 Temperature
               </h2>
             </div>
             <div>
-              <p class="mt-6 mb-2 text-lg leading-8 text-black-600 text-center">
+              <p className="mt-6 mb-2 text-lg leading-8 text-black-600 text-center">
                 Upper Limit
               </p>
-              <div class="relative">
-                <span class="absolute inset-y-2 right-10 flex items-center pr-1 text-gray-500">
+              <div className="relative">
+                <span className="absolute inset-y-2 right-10 flex items-center pr-1 text-gray-500">
                   °C
                 </span>
                 <input
@@ -146,116 +147,116 @@ const Limits = () => {
                   type="text"
                   pattern="[0-9]*"
                   inputmode="numeric"
-                  class=" mx-auto block w-1/2 rounded-md border-0 py-1.5 pl-2 pr-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className=" mx-auto block w-1/2 rounded-md border-0 py-1.5 pl-2 pr-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 ></input>
               </div>
 
-              <p class="mt-2 text-lg leading-8 text-gray-400 text-center">
+              <p className="mt-2 text-lg leading-8 text-gray-400 text-center">
                 Recommended Limit: 24 °C
               </p>
             </div>
 
             <div>
-              <p class="mt-6 mb-2 text-lg leading-8 text-black-600 text-center">
+              <p className="mt-6 mb-2 text-lg leading-8 text-black-600 text-center">
                 Lower Limit
               </p>
-              <div class="relative">
-                <span class="absolute inset-y-2 right-10 flex items-center pr-1 text-gray-500">
+              <div className="relative">
+                <span className="absolute inset-y-2 right-10 flex items-center pr-1 text-gray-500">
                   °C
                 </span>
                 <input
                   value={LTemp}
                   onChange={handleLTempChange}
                   type="text"
-                  class="mx-auto block w-1/2 rounded-md border-0 py-1.5 pl-2 pr-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="mx-auto block w-1/2 rounded-md border-0 py-1.5 pl-2 pr-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 ></input>
               </div>
 
-              <p class="mt-2 text-lg leading-8 text-gray-400 text-center">
+              <p className="mt-2 text-lg leading-8 text-gray-400 text-center">
                 Recommended Limit: 18 °C
               </p>
             </div>
           </div>
-          <div class=" border max-w-2xl space-y-16 rounded-md px-3.5 py-2.5 text-sm ">
+          <div className=" border max-w-2xl space-y-16 rounded-md px-3.5 py-2.5 text-sm ">
             <div>
-              <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center">
+              <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center">
                 Humidity
               </h2>
             </div>
             <div>
-              <p class="mt-6 mb-2 text-lg leading-8 text-black-600 text-center">
+              <p className="mt-6 mb-2 text-lg leading-8 text-black-600 text-center">
                 Upper Limit
               </p>
-              <div class="relative">
-                <span class="absolute inset-y-2 right-10 flex items-center pr-1 text-gray-500">
+              <div className="relative">
+                <span className="absolute inset-y-2 right-10 flex items-center pr-1 text-gray-500">
                   %
                 </span>
                 <input
                   value={UHum}
                   onChange={handleUHumChange}
                   type="text"
-                  class="mx-auto block w-1/2 rounded-md border-0 py-1.5 pl-2 pr-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="mx-auto block w-1/2 rounded-md border-0 py-1.5 pl-2 pr-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 ></input>
               </div>
 
-              <p class="mt-2 text-lg leading-8 text-gray-400 text-center">
+              <p className="mt-2 text-lg leading-8 text-gray-400 text-center">
                 Recommended Limit: 60 %
               </p>
             </div>
 
             <div>
-              <p class="mt-6 mb-2 text-lg leading-8 text-black-600 text-center">
+              <p className="mt-6 mb-2 text-lg leading-8 text-black-600 text-center">
                 Lower Limit
               </p>
-              <div class="relative">
-                <span class="absolute inset-y-2 right-10 flex items-center pr-1 text-gray-500">
+              <div className="relative">
+                <span className="absolute inset-y-2 right-10 flex items-center pr-1 text-gray-500">
                   %
                 </span>
                 <input
                   value={LHum}
                   onChange={handleLHumChange}
                   type="text"
-                  class="mx-auto block w-1/2 rounded-md border-0 py-1.5 pl-2 pr-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="mx-auto block w-1/2 rounded-md border-0 py-1.5 pl-2 pr-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 ></input>
               </div>
 
-              <p class="mt-2 text-lg leading-8 text-gray-400 text-center">
+              <p className="mt-2 text-lg leading-8 text-gray-400 text-center">
                 Recommended Limit: 30 %
               </p>
             </div>
           </div>
-          <div class=" border max-w-2xl space-y-16 rounded-md px-3.5 py-2.5 text-sm ">
+          <div className=" border max-w-2xl space-y-16 rounded-md px-3.5 py-2.5 text-sm ">
             <div>
-              <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center">
+              <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center">
                 CO2
               </h2>
             </div>
             <div>
-              <p class="mt-12 mb-2 text-lg leading-8 text-black-600 text-center">
+              <p className="mt-12 mb-2 text-lg leading-8 text-black-600 text-center">
                 Upper Limit
               </p>
-              <div class="relative">
-                <span class="absolute inset-y-2 right-5 flex items-center pr-1 text-gray-500">
+              <div className="relative">
+                <span className="absolute inset-y-2 right-5 flex items-center pr-1 text-gray-500">
                   ppm
                 </span>
                 <input
                   value={CO2}
                   onChange={handleCO2Change}
                   type="text"
-                  class="mx-auto block w-1/2 rounded-md border-0 py-1.5 pl-2 pr-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="mx-auto block w-1/2 rounded-md border-0 py-1.5 pl-2 pr-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 ></input>
               </div>
-              <p class="mt-2 text-lg leading-8 text-gray-400 text-center">
+              <p className="mt-2 text-lg leading-8 text-gray-400 text-center">
                 Recommended Limit: 800 ppm
               </p>
             </div>
           </div>
         </div>
-        <div class="flex justify-center ">
+        <div className="flex justify-center ">
           <button
             type="submit"
             onClick={handleSubmit}
-            class="mt-7 w-96 h-14 items-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="mt-7 w-96 h-14 items-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Save Limits
           </button>
