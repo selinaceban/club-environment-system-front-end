30de1259cdac19fffc9da1a40f620e2ea949b48a
Daniel <39463084+dosermind@users.noreply.github.com>
2023-05-31 01:29:20 +0200

    fixed some spacing

--- src/NavBarComponent.js
+++ src/NavBarComponent.js
@@ -14,55 +14,54 @@ const NavBarComponent = () => {
       {/* Nav bar */}
       <nav className="bg-gray-800 py-4">
 
-          <div className="flex items-center justify-between">
-            {/* Toggle icon button */}
-            <button className="ml-5 text-white" onClick={toggleSidebar}>
+        <div className="flex items-center justify-between">
+          {/* Toggle icon button */}
+          <button className="ml-5 text-white" onClick={toggleSidebar}>
 
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-              </svg>
-            </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </button>
 
-            <h1 className="text-white text-2xl font-bold">
-              Venue Environment Management
-            </h1>
-            <div
-              className={`${
-                isSidebarOpen ? "block" : "hidden"
+          <h1 className="text-white text-2xl font-bold">
+            Venue Environment Management
+          </h1>
+          <div
+            className={`${isSidebarOpen ? "block" : "hidden"
               } md:block  md:mt-0 mx-5`}
 
+          >
+            <a
+              href="ReadingsComponent"
+              className="text-gray-300 hover:text-white text-base md:mx-4"
             >
-              <a
-                href="ReadingsComponent"
-                className="text-gray-300 hover:text-white text-base md:mx-4"
-              >
-                Readings
-              </a>
-              <a
-                href="LimitsComponent"
-                className="text-gray-300 hover:text-white text-base md:mx-4 "
-              >
-                Limits
-              </a>
-              <a
-                href="LogTableComponent"
-                className="text-gray-300 hover:text-white text-base md:mx-4"
-              >
-                Logs
-              </a>
-            </div>
+              Readings
+            </a>
+            <a
+              href="LimitsComponent"
+              className="text-gray-300 hover:text-white text-base md:mx-4 "
+            >
+              Limits
+            </a>
+            <a
+              href="LogTableComponent"
+              className="text-gray-300 hover:text-white text-base md:mx-4"
+            >
+              Logs
+            </a>
           </div>
+        </div>
 
       </nav>
       {/* Sidebar */}
@@ -73,50 +72,50 @@ const NavBarComponent = () => {
             <Typography variant="h5" color="blue-gray">
               Sidebar
             </Typography>
-            </div>
-            <div className="border-t-2 border-gray-800">
-              <ul>
-                <li>
-                  <div className="p-4 rounded-lg">
-                    <a
-                      href="ReadingsComponent"
-                      className="text-xl  hover:text-grey "
-                    >
-                      Readings
-                    </a>
-                  </div>
-                </li>
-                <li>
-                  <div className="p-4 rounded-lg">
-                    <a
-                      href="LimitsComponent"
-                      className="text-xl hover:text-grey "
-                    >
-                      Limits
-                    </a>
-                  </div>
-                </li>
-                <li>
-                  <div className="p-4 rounded-lg">
-                    <a
-                      href="LogTableComponent"
-                      className="text-xl hover:text-grey "
-                    >
-                      Logs
-                    </a>
-                  </div>
-                </li>
-                <li>
-                  <ClimateControlToggleComponent />
-                </li>
-              </ul>
-            </div>
-          </Card>
-        </div>
-
-        {/* Content */}
-        <div className="flex-1">{/* Your page content goes here */}</div>
+          </div>
+          <div className="border-t-2 border-gray-800">
+            <ul>
+              <li>
+                <div className="p-4 rounded-lg">
+                  <a
+                    href="ReadingsComponent"
+                    className="text-xl  hover:text-grey "
+                  >
+                    Readings
+                  </a>
+                </div>
+              </li>
+              <li>
+                <div className="p-4 rounded-lg">
+                  <a
+                    href="LimitsComponent"
+                    className="text-xl hover:text-grey "
+                  >
+                    Limits
+                  </a>
+                </div>
+              </li>
+              <li>
+                <div className="p-4 rounded-lg">
+                  <a
+                    href="LogTableComponent"
+                    className="text-xl hover:text-grey "
+                  >
+                    Logs
+                  </a>
+                </div>
+              </li>
+              <li>
+                <ClimateControlToggleComponent />
+              </li>
+            </ul>
+          </div>
+        </Card>
       </div>
+
+      {/* Content */}
+      <div className="flex-1">{/* Your page content goes here */}</div>
+    </div>
   );
 };
 
