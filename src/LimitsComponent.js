ccb178788c28619d4be9c33a9d6c33d615e46695
Daniel <39463084+dosermind@users.noreply.github.com>
2023-05-31 01:41:21 +0200

    changes button colors so all look the same blue

--- src/LimitsComponent.js
+++ src/LimitsComponent.js
@@ -296,7 +296,7 @@ const Limits = () => {
           <button
             type="submit"
             onClick={handleSubmit}
-            className="mt-7 w-96 h-14 items-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="mt-7 w-96 h-14 items-center rounded-md bg-blue-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Save Limits
           </button>
